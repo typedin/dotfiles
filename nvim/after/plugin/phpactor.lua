a99f54d39f5b97453a9503d36d51c8e929f7eeb1
Antoine Cheynet <antoine@typedin.dev>
2022-12-18 22:22:38 +0100

    phpactor tweak install process

--- nvim/after/plugin/phpactor.lua
+++ nvim/after/plugin/phpactor.lua
@@ -1,16 +1,37 @@
-require("phpactor").setup {
+-- vim.fn.stdpath('data') == ~/.local/share/nvim/site/
+local install_path = vim.fn.stdpath('data') .. '/opt/phpactor/'
+
+local function check_for_executable()
+    local f = io.open(install_path, 'r')
+    if f == nil then
+        return false
+    end
+    -- check if the file is executable by checking x in its mods
+    --
+    local attributes = require('lfs').attributes(install_path .. '/bin/phpactor')
+    return string.find(attributes['permissions'], 'x')
+end
+
+if not check_for_executable() then
+    print('phpactor is not executable')
+    print('try running these commands:')
+    print('git clone git@github.com:phpactor/phpactor ' .. install_path)
+    print('(see: https://phpactor.readthedocs.io/en/master/usage/standalone.html)')
+end
+
+require('phpactor').setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     install = {
-        path = vim.fn.stdpath "data" .. "/opt/",
-        branch = "master",
+        path = install_path,
+        branch = 'master',
         -- this is : ~/.local/share/nvim/opt/phpactor
-        bin = vim.fn.stdpath "data" .. "/opt/phpactor/bin/phpactor",
-        php_bin = "php",
-        composer_bin = "composer",
-        git_bin = "git",
-        check_on_startup = "none",
+        bin = install_path .. '/bin/phpactor',
+        php_bin = 'php',
+        composer_bin = 'composer',
+        git_bin = 'git',
+        check_on_startup = 'none',
     },
     lspconfig = {
         enabled = true,
@@ -18,4 +39,4 @@ require("phpactor").setup {
             language_server_phpstan_enabled = true,
         },
     },
-}
+})
