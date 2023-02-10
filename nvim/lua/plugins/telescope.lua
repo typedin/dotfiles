9e3590665e2da1a4feb34ccd688a9b567cacfd11
typedin <antoine@typedin.dev>
2023-02-10 16:08:47 +0100

    install php actor through lazy.nvim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-lua/plenary.nvim',
+        'nvim-lua/popup.nvim',
+        'nvim-telescope/telescope-file-browser.nvim',
+        'nvim-telescope/telescope-fzf-writer.nvim',
+        'nvim-telescope/telescope-hop.nvim',
+        'nvim-telescope/telescope-packer.nvim',
+        'nvim-telescope/telescope-rs.nvim',
+        'nvim-telescope/telescope-smart-history.nvim',
+        'nvim-telescope/telescope-symbols.nvim',
+        'nvim-telescope/telescope-ui-select.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+}
