7488dfa17674cefc0cdabb9e8567f63fa40bc3f2
typedin <antoine@typedin.dev>
2023-02-10 09:18:06 +0100

    tweaked ufo display

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -1,6 +1,6 @@
 return {
     'luukvbaal/statuscol.nvim',
     config = function()
-        require('statuscol').setup()
+        require('statuscol').setup({ foldfunc = 'builtin', setopt = true })
     end,
 }
