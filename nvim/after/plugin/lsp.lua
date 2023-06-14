0d5aa7c3ea4204a235bdd5b44f6ab86f21da44b2
typedin <antoine@typedin.dev>
2023-06-14 18:23:23 +0200

    use return as selector for auto complete

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -49,7 +49,7 @@ cmp.setup({
     }),
     mapping = {
         -- `Enter` key to confirm completion
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         -- Ctrl+Space to trigger completion menu
         ['<C-Space>'] = cmp.mapping.complete(),
         -- Navigate between snippet placeholder
