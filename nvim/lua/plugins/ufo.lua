2f66ba397b8c7f0c69710562d8c5eb9311b3c93b
typedin <antoine@typedin.dev>
2023-07-11 23:43:44 +0200

    ufo stuff in one place

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -25,6 +25,7 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
     table.insert(newVirtText, { suffix, 'MoreMsg' })
     return newVirtText
 end
+
 return {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
@@ -35,5 +36,15 @@ return {
                 return { 'treesitter', 'indent' }
             end,
         })
+    end,
+    init = function()
+        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+
+        -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
     end,
 }
