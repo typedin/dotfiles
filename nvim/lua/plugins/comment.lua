0977a2c1b1a84543cbd01cbc6b8001ccc51d61af
typedin <antoine@typedin.dev>
2023-09-19 23:31:12 +0200

    lazy load comment

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,5 +1,6 @@
 return {
     'numToStr/Comment.nvim',
+    lazy = false,
     dependencies = {
         'JoosepAlviste/nvim-ts-context-commentstring',
     },
@@ -7,8 +8,8 @@ return {
         require('Comment').setup({
             pre_hook = function(ctx)
                 local U = require('Comment.utils')
-
                 local location = nil
+
                 if ctx.ctype == U.ctype.block then
                     location = require('ts_context_commentstring.utils').get_cursor_location()
                 elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
