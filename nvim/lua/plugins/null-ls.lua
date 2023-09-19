808c0b0523d51c396ac39d763bb1586c9047af2a
typedin <antoine@typedin.dev>
2023-09-19 23:06:04 +0200

    remove format behavior from null-ls

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,6 +1,6 @@
 --[[
-    as per this https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621 
-     null-ls is not maintained anymore
+    as per this https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1621
+    null-ls is not maintained anymore
 ]]
 local function has_eslint_config(utils)
     return utils.root_has_file({
@@ -14,24 +14,25 @@ local function has_eslint_config(utils)
     })
 end
 
-return {
-    'jose-elias-alvarez/null-ls.nvim',
-    dependencies = {
-        'jayp0521/mason-null-ls.nvim',
-        'ThePrimeagen/refactoring.nvim',
-    },
-    config = function()
-        require('null-ls').setup({
-            sources = {
-                require('null-ls').builtins.diagnostics.eslint_d.with({ condition = has_eslint_config }),
-                require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
-                require('null-ls').builtins.formatting.eslint_d.with({ condition = has_eslint_config }),
-                require('null-ls').builtins.diagnostics.luacheck,
-                require('null-ls').builtins.formatting.prettierd,
-                require('null-ls').builtins.code_actions.refactoring,
-                require('null-ls').builtins.formatting.phpcsfixer,
-            },
-        })
-        require('mason-null-ls').setup({ automatic_installation = true })
-    end,
-}
+return {}
+--[[ return { ]]
+--[[     'jose-elias-alvarez/null-ls.nvim', ]]
+--[[     dependencies = { ]]
+--[[         'jayp0521/mason-null-ls.nvim', ]]
+--[[         'ThePrimeagen/refactoring.nvim', ]]
+--[[     }, ]]
+--[[     config = function() ]]
+--[[         require('null-ls').setup({ ]]
+--[[             sources = { ]]
+--[[                 require('null-ls').builtins.diagnostics.eslint_d.with({ condition = has_eslint_config }), ]]
+--[[                 require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }), ]]
+--[[                 require('null-ls').builtins.formatting.eslint_d.with({ condition = has_eslint_config }), ]]
+--[[                 require('null-ls').builtins.formatting.prettierd, ]]
+--[[                 require('null-ls').builtins.formatting.phpcsfixer, ]]
+--[[                 require('null-ls').builtins.diagnostics.luacheck, ]]
+--[[                 require('null-ls').builtins.code_actions.refactoring, ]]
+--[[             }, ]]
+--[[         }) ]]
+--[[         require('mason-null-ls').setup({ automatic_installation = true }) ]]
+--[[     end, ]]
+--[[ } ]]
