6ed7c193508d7fd87f79b2377447d3c116840b20
typedin <antoine@typedin.dev>
2024-01-31 13:07:49 +0100

    got lsp back for js

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,38 +1,45 @@
 return {
-    "nvimtools/none-ls.nvim",
-    "lukas-reineke/lsp-format.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+    {
+        'mhartington/formatter.nvim',
+    },
+    {
+        "lukas-reineke/lsp-format.nvim",
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.blade_formatter,
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.completion.spell,
-            }
-        })
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.formatting.blade_formatter,
+                    null_ls.builtins.diagnostics.eslint_d,
+                    null_ls.builtins.completion.spell,
+                }
+            })
 
-        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-        null_ls.setup({
-            -- you can reuse a shared lspconfig on_attach callback here
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                            vim.lsp.buf.formatting_sync()
-                        end,
-                    })
-                end
-            end,
-        })
-    end,
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            null_ls.setup({
+                -- you can reuse a shared lspconfig on_attach callback here
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                                vim.lsp.buf.formatting_sync()
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
     {
         'stevearc/conform.nvim',
         event = {
