82860f32d9634f9ed28f55f4d7d97f7bb871f357
typedin <antoine@typedin.dev>
2024-01-31 11:30:29 +0100

    tweaked git integration

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,20 +1,14 @@
 return {
-    'tpope/vim-fugitive',
-    opts = {},
     {
         'lewis6991/gitsigns.nvim',
         config = function()
-            require('gitsigns').setup({
-                signcolumn = true,
-                signs = {
-                    add = { text = '+' },
-                    change = { text = '~' },
-                    delete = { text = '_' },
-                    topdelete = { text = '-' },
-                    changedelete = { text = '~' },
-                },
-            })
+            require('gitsigns').setup()
+            vim.keymap.set('n', '<localleader>gp', ':Gitsigns preview_hunk<CR>', {})
+            vim.keymap.set('n', '<localleader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
         end,
+    },
+    {
+        'tpope/vim-fugitive',
     },
     {
         'akinsho/git-conflict.nvim',
