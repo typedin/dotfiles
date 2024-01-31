e3eaa88f4dff0d5d4577709a05882bf0fd4e6ed5
typedin <antoine@typedin.dev>
2024-01-31 11:40:24 +0100

    test: WIP

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -2,7 +2,6 @@ return {
     'vim-test/vim-test',
     dependencies = {
         'kana/vim-vspec',
-        'kana/vim-vspec',
         'neomake/neomake',
         'tpope/vim-dispatch',
         'voldikss/vim-floaterm',
