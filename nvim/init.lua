2628e6d7645fcc17b3a7a856ac661c7cdf21dd1d
typedin <antoine@typedin.dev>
2024-12-11 22:51:54 +0100

    simplified stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,90 +1,17 @@
+local set = vim.keymap.set
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
+---@diagnostic disable-next-line: inject-field
 vim.g.mapleader = " "
+---@diagnostic disable-next-line: inject-field
 vim.g.maplocalleader = ","
 
--- Quickly go to latest buffer
-vim.keymap.set("n", "<LocalLeader><LocalLeader>", "<C-^>")
-
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move lines up
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move lines down
-
--- When test is wrapped, moved by terminal rows, not lines, unless a count is provided
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
---
-vim.keymap.set("n", "J", "mzJ`z") -- keep cursor in place
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- half page jumping down but keep cursor in place
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- half page jumping up but keep cursor in place
-vim.keymap.set("n", "n", "nzzzv") -- keep cursor in the middle when cycling down search
-vim.keymap.set("n", "N", "Nzzzv") -- keep cursor in the middle when cycling down search
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
---
 -- Set to true if you have a Nerd Font installed
+---@diagnostic disable-next-line: inject-field
 vim.g.have_nerd_font = true
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, for help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = "a"
-
--- Don't show the mode, since it's already in status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = "unnamedplus"
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = "yes"
-
--- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = "split"
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
 
 local shortmessage_options = {
     "W", -- don't echo "[w]"/"[written]" when writing
@@ -103,62 +30,94 @@ for _, value in pairs(shortmessage_options) do
     vim.opt.shortmess:append(value)
 end
 
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
 local options = {
-    guicursor = "",
-    emoji = false, -- don't assume all emoji are double width
-    relativenumber = true,
-    number = true,
-    scrolloff = 8,
-    expandtab = true,
-    shiftwidth = 4,
-    tabstop = 4,
-    softtabstop = 4,
-    smartindent = true,
-    wrap = true,
-    showbreak = "↳",
-    swapfile = false,
-    undodir = os.getenv("HOME") .. "/.vim/undodir",
-    undofile = true, -- persistent mode
     backup = false, -- automatically save a backup file
-    dir = "/tmp/",
-    confirm = true, -- ask for confirmation instead of erroring
-    hlsearch = false,
-    incsearch = true,
-    termguicolors = true,
-    signcolumn = "yes",
-    updatetime = 50,
-    splitbelow = false, -- open horizontal splits below current window,
-    splitright = true, -- open vertical splits to the right of the current window
-    spell = false,
-    shortmess = vim.opt.shortmess + { c = true },
-    title = true,
-    wildmode = "longest:full,full", -- complete the longest common mathc, and allow tabbing the results to fully complete them
-    completeopt = "menuone,longest,preview",
+    breakindent = true, -- Enable break indent
+    clipboard = "unnamedplus",
+    cursorline = true,
     cmdheight = 0,
-    ignorecase = true, -- Search
-    smartcase = true, -- if I type a search with capital letter the search becomes case sensitive
-    list = true,
+    completeopt = "menuone,longest,preview",
+    confirm = true, -- ask for confirmation instead of erroring
+    dir = "/tmp/",
+    emoji = false, -- don't assume all emoji are double width
+    expandtab = true,
+    guicursor = "",
+    hlsearch = true, -- Set highlight on search, but clear on pressing <Esc> in normal mode
+    inccommand = "split", -- Preview substitutions live, as you type!
+    incsearch = true,
+    list = true, -- Sets how neovim will display certain whitespace in the editor.
     listchars = {
         tab = "➣ ", -- U+27A3
         trail = "•",
         nbsp = "⦸", -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
     },
+    mouse = "a",
+    number = true,
+    relativenumber = true,
+    scrolloff = 8, -- Minimal number of screen lines to keep above and below the cursor.
+    shiftwidth = 4,
+    shortmess = vim.opt.shortmess + { c = true },
+    showmode = false, -- Don't show the mode, since it's already in status line
+    showbreak = "↳",
+    signcolumn = "yes",
+    smartindent = true,
+    softtabstop = 4,
+    spell = false,
+    splitbelow = false, -- open horizontal splits below current window,
+    splitright = true, -- open vertical splits to the right of the current window
+    swapfile = false,
+    tabstop = 4,
+    termguicolors = true,
+    timeoutlen = 300,
+    title = true,
+    undodir = os.getenv("HOME") .. "/.vim/undodir",
+    undofile = true, -- persistent mode
+    updatetime = 50,
+    wildmode = "longest:full,full", -- complete the longest common mathc, and allow tabbing the results to fully complete them
+    wrap = true,
 }
 for option, value in pairs(options) do
     vim.opt[option] = value
 end
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+--
+-- Quickly go to latest buffer
+set("n", "<LocalLeader><LocalLeader>", "<C-^>")
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+set("v", "K", ":m '<-2<CR>gv=gv") -- move lines up
+set("v", "J", ":m '>+1<CR>gv=gv") -- move lines down
+
+-- When test is wrapped, moved by terminal rows, not lines, unless a count is provided
+set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+set("n", "J", "mzJ`z") -- keep cursor in place
+set("n", "<C-d>", "<C-d>zz") -- half page jumping down but keep cursor in place
+set("n", "<C-u>", "<C-u>zz") -- half page jumping up but keep cursor in place
+set("n", "n", "nzzzv") -- keep cursor in the middle when cycling down search
+set("n", "N", "Nzzzv") -- keep cursor in the middle when cycling down search
+
+set("n", "<C-k>", "<cmd>cnext<CR>zz")
+set("n", "<C-j>", "<cmd>cprev<CR>zz")
+set("n", "<leader>k", "<cmd>lnext<CR>zz")
+set("n", "<leader>j", "<cmd>lprev<CR>zz")
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+set("n", "[d", function()
+    vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Go to [P]revious [D]iagnostic message" })
+set("n", "]d", function()
+    vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Go to [N]ext [D]iagnostic message" })
+set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -166,22 +125,16 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -6,6 +6,8 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
+        -- Useful for getting pretty icons, but requires a Nerd Font.
+        { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "make",
@@ -13,52 +15,18 @@ return {
                 return vim.fn.executable("make") == 1
             end,
         },
-
-        -- Useful for getting pretty icons, but requires a Nerd Font.
-        { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
     },
     config = function()
-        -- Telescope is a fuzzy finder that comes with a lot of different things that
-        -- it can fuzzy find! It's more than just a "file finder", it can search
-        -- many different aspects of Neovim, your workspace, LSP, and more!
-        --
-        -- The easiest way to use telescope, is to start by doing something like:
-        --  :Telescope help_tags
-        --
-        -- After running this command, a window will open up and you're able to
-        -- type in the prompt window. You'll see a list of help_tags options and
-        -- a corresponding preview of the help.
-        --
-        -- Two important keymaps to use while in telescope are:
-        --  - Insert mode: <c-/>
-        --  - Normal mode: ?
-        --
-        -- This opens a window that shows you all of the keymaps for the current
-        -- telescope picker. This is really useful to discover what Telescope can
-        -- do as well as how to actually do it!
-
-        -- [[ Configure Telescope ]]
-        -- See `:help telescope` and `:help telescope.setup()`
         require("telescope").setup({
-            -- You can put your default mappings / updates / etc. in here
-            --  All the info you're looking for is in `:help telescope.setup()`
-            --
-            -- defaults = {
-            --   mappings = {
-            --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-            --   },
-            -- },
-            -- pickers = {}
             extensions = {
+                fzf = {},
                 ["ui-select"] = {
                     require("telescope.themes").get_dropdown(),
                 },
             },
         })
 
-        -- Enable telescope extensions, if they are installed
-        pcall(require("telescope").load_extension, "fzf")
-        pcall(require("telescope").load_extension, "ui-select")
+        -- pcall(require("telescope").load_extension, "ui-select")
 
         -- See `:help telescope.builtin`
         local builtin = require("telescope.builtin")
