9b8036fc40df16a7f8f1d4bb50c18d1e1802488a
typedin <antoine@typedin.dev>
2024-12-11 23:55:24 +0100

    wezterm navigation

--- nvim/lua/typedin/keymaps.lua
+++ nvim/lua/typedin/keymaps.lua
@@ -40,12 +40,3 @@ end, { desc = "Go to [N]ext [D]iagnostic message" })
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
