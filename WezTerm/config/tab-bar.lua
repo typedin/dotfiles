0ed3a8ce78f79ef8521b0850ef39b9fa42125bae
typedin <antoine@typedin.dev>
2024-12-13 22:57:33 +0100

    use same theme in wezterm and nvim

--- WezTerm/config/tab-bar.lua
+++ WezTerm/config/tab-bar.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 M.enable_tab_bar = false
+M.tab_bar_at_bottom = true
 M.hide_tab_bar_if_only_one_tab = false
 M.show_new_tab_button_in_tab_bar = true
 M.show_tab_index_in_tab_bar = false
 M.show_tabs_in_tab_bar = true
 M.switch_to_last_active_tab_when_closing_tab = false
 M.tab_and_split_indices_are_zero_based = false
-M.tab_bar_at_bottom = true
-M.tab_max_width = 25
+M.tab_max_width = 45
 M.use_fancy_tab_bar = false
 
 return M
