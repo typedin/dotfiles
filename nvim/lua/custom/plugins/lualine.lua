b617891cc8d07ccd26a5ff79a65af63166b94abf
typedin <antoine@typedin.dev>
2024-12-17 12:51:45 +0100

    lualine loads its scheme from globaly assigned theme

--- nvim/lua/custom/plugins/lualine.lua
+++ nvim/lua/custom/plugins/lualine.lua
@@ -18,7 +18,6 @@ return {
                     -- Set opacity for cursorline and number background
                     line_opacity = 0.15,
                     -- Enable cursor highlights
-                    set_cursor = true,
                     -- Enable cursorline initially, and disable cursorline for inactive windows
                     -- or ignored filetypes
                     set_cursorline = true,
