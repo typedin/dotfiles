402dd96a4ba763e2917b4891ff4c863662bb76da
typedin <antoine@typedin.dev>
2024-12-17 12:41:09 +0100

    cleaned diagnostics setup

--- nvim/lua/custom/config.lua
+++ nvim/lua/custom/config.lua
@@ -1,19 +1,32 @@
 local signs = require("custom.signs")
 
 vim.diagnostic.config({
-    virtual_text = false,
     float = {
         source = "always", -- Or "if_many"
+        scope = "cursor", -- {scope} ('line'|'buffer'|'cursor'|'c'|'l'|'b', default: line) Show diagnostics from the whole buffer (buffer", the current cursor line (line), or the current cursor position (cursor). Shorthand versions are also accepted (c for cursor, l for line, b for buffer).
     },
+    severity_sort = true,
     signs = {
+        linehl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.HINT] = "ErrorMsg",
+            [vim.diagnostic.severity.INFO] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarnMsg",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.HINT] = "ErrorMsg",
+            [vim.diagnostic.severity.INFO] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "ErrorMsg",
+        },
         text = {
-            [vim.diagnostic.severity.HINT] = signs.hint,
             [vim.diagnostic.severity.ERROR] = signs.error,
-            [vim.diagnostic.severity.WARN] = signs.warn,
+            [vim.diagnostic.severity.HINT] = signs.hint,
             [vim.diagnostic.severity.INFO] = signs.info,
+            [vim.diagnostic.severity.WARN] = signs.warn,
         },
     },
     underline = true,
     update_in_insert = false,
-    severity_sort = true,
+    virtual_text = false,
 })
