d15a4678859ff0653113cd3f5aad72bd17070c6f
typedin <antoine@typedin.dev>
2024-12-27 20:52:48 +0100

    use tokyo theme for zsh

---
+++ zsh/themes/tokyo-night.zsh
@@ -0,0 +1,42 @@
+# Tokyo Night Colors Lookup Table
+typeset -A colors=(
+    "bg" "#222436"
+    "bg_dark" "#1e2030"
+    "bg_dark1" "#191B29"
+    "bg_highlight" "#2f334d"
+    "blue" "#82aaff"
+    "blue0" "#3e68d7"
+    "blue1" "#65bcff"
+    "blue2" "#0db9d7"
+    "blue5" "#89ddff"
+    "blue6" "#b4f9f8"
+    "blue7" "#394b70"
+    "command" "#636da6"
+    "comment" "#636da6"
+    "cyan" "#86e1fc"
+    "dark2" "#636da6"
+    "dark3" "#545c7e"
+    "dark5" "#737aa2"
+    "fg" "#c8d3f5"
+    "fg_dark" "#828bb8"
+    "fg_gutter" "#3b4261"
+    "green" "#c3e88d"
+    "green1" "#4fd6be"
+    "green2" "#41a6b5"
+    "magenta" "#c099ff"
+    "magenta2" "#ff007c"
+    "orange" "#ff966c"
+    "purple" "#fca7ea"
+    "red" "#ff757f"
+    "red1" "#c53b53"
+    "teal" "#4fd6be"
+    "terminal_black" "#444a73"
+    "yellow" "#ffc777"
+)
+
+typeset -A git_colors=(
+    "add" "#b8db87"
+    "change" "#7ca1f2"
+    "delete" "#e26a75"
+)
+
