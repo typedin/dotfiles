0d893c2214bcde8e8ba95b07dbd8f9a4f1d10410
typedin <antoine@typedin.dev>
2025-01-08 23:08:12 +0100

    at least I have vim api suggestions

--- nvim/lua/custom/plugins/autocomplete.lua
+++ nvim/lua/custom/plugins/autocomplete.lua
@@ -7,18 +7,24 @@ return {
             -- Snippet Engine & its associated nvim-cmp source
             {
                 "L3MON4D3/LuaSnip",
-                build = (function()
-                    -- Build Step is needed for regex support in snippets
-                    if vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
+                -- follow latest release.
+                version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+                -- install jsregexp (optional!).
+                build = "make install_jsregexp",
             },
             "saadparwaiz1/cmp_luasnip",
+
+            "neovim/nvim-lspconfig",
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
-            "github/copilot.vim",
+            "hrsh7th/cmp-emoji",
+            {
+                "zbirenbaum/copilot-cmp",
+                config = function()
+                    require("copilot_cmp").setup()
+                end,
+            },
 
             -- If you want to add a bunch of pre-configured snippets,
             --    you can use this plugin to help you. It even has snippets
@@ -31,25 +37,60 @@ return {
             local cmp = require("cmp")
             local luasnip = require("luasnip")
             luasnip.config.setup({})
-
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        luasnip.lsp_expand(args.body)
+                        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+                        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        -- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    end,
+                },
+                sorting = {
+                    priority_weight = 2,
+                    comparators = {
+                        require("copilot_cmp.comparators").prioritize,
+
+                        -- Below is the default comparitor list and order for nvim-cmp
+                        cmp.config.compare.offset,
+                        -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+                        cmp.config.compare.exact,
+                        cmp.config.compare.score,
+                        cmp.config.compare.recently_used,
+                        cmp.config.compare.locality,
+                        cmp.config.compare.kind,
+                        cmp.config.compare.sort_text,
+                        cmp.config.compare.length,
+                        cmp.config.compare.order,
+                    },
+                },
+                formatting = {
+                    -- See: https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
+                    format = function(entry, vim_item)
+                        -- Set `kind` to "$icon $kind".
+                        local lsp_kinds = require("custom.signs").lsp_kinds
+                        vim_item.kind = string.format("%s %s", lsp_kinds[vim_item.kind], vim_item.kind)
+                        vim_item.menu = ({
+                            buffer = "[Buffer]",
+                            nvim_lsp = "[LSP]",
+                            luasnip = "[LuaSnip]",
+                            nvim_lua = "[Lua]",
+                            latex_symbols = "[LaTeX]",
+                        })[entry.source.name]
+                        return vim_item
                     end,
                 },
                 completion = { completeopt = "menu,menuone,noinsert" },
-
                 -- For an understanding of why these mappings were
                 -- chosen, you will need to read `:help ins-completion`
-                --
                 -- No, but seriously. Please read `:help ins-completion`, it is really good!
                 mapping = cmp.mapping.preset.insert({
                     -- Select the [n]ext item
                     ["<C-n>"] = cmp.mapping.select_next_item(),
                     -- Select the [p]revious item
                     ["<C-p>"] = cmp.mapping.select_prev_item(),
-
                     -- Accept ([y]es) the completion.
                     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 }),
@@ -57,7 +98,32 @@ return {
                     { name = "copilot" },
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
+                    { name = "nvim_lua" },
+                    { name = "buffer" },
+                    { name = "calc" },
+                    { name = "emoji" },
                     { name = "path" },
+                },
+                window = {
+                    completion = cmp.config.window.bordered({
+                        border = "rounded",
+                        col_offset = -1,
+                        scrollbar = false,
+                        scrolloff = 3,
+                        -- Default for bordered() is 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None'
+                        -- Default for non-bordered, which we'll use here, is:
+                        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+                    }),
+                    documentation = cmp.config.window.bordered({
+                        border = "rounded",
+                        scrollbar = false,
+                        -- Default for bordered() is 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None'
+                        -- Default for non-bordered is 'FloatBorder:NormalFloat'
+                        -- Suggestion from: https://github.com/hrsh7th/nvim-cmp/issues/2042
+                        -- is to use 'NormalFloat:NormalFloat,FloatBorder:FloatBorder,CursorLine:Visual,Search:None'
+                        -- but this also seems to suffice:
+                        winhighlight = "CursorLine:Visual,Search:None",
+                    }),
                 },
             })
         end,
