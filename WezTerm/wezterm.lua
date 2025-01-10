02af49a08d3f8b5a1e07982a45480b0530a6ecbb
typedin <antoine@typedin.dev>
2025-01-10 21:16:11 +0100

    navigation between NeoVim and Wezterm is handled bu wezterm

--- WezTerm/wezterm.lua
+++ WezTerm/wezterm.lua
@@ -1,6 +1,7 @@
 local appearance = require("config")
 local utils = require("utils.functions")
 local wezterm = require("wezterm")
+require("plugins")
 -- In newer versions of wezterm, use the config_builder which will help provide clearer error messages
 local config = wezterm.config_builder()
 
